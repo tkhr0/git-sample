b5a530aca8d14baa3d6a59f71c49cbb07dbcef11
KarasawaTakahiro <karasawa@basicinc.jp>
2020-09-25 11:26:27 +0900

    client feature

--- client.rb
+++ client.rb
@@ -1,2 +1,18 @@
+require_relative './connection'
+
 class Client
+  def initialize username
+    @username = username
+    connect
+  end
+
+  def send message
+    @connection.send(@username, message)
+  end
+
+  private
+
+  def connect
+    @connection = Connection.new('192.168.1.1')
+  end
 end
