60766044811371f20c85435160b5e9165ad0ff49
KarasawaTakahiro <karasawa@basicinc.jp>
2020-09-25 11:45:46 +0900

    server feature

--- connection.rb
+++ connection.rb
@@ -1,9 +1,40 @@
 class Connection
+  @@clients = []
+
   def initialize server_ip
     @ip = server_ip
+    bind
+  end
+
+  def bind
+    puts "bind port #{@ip}:2345"
+  end
+
+  def connect ip
+    @@clients << ip
+    true
   end
 
   def send username, message
     puts "send #{@ip}: [#{username}] #{message}"
+  end
+
+  def receive
+    return [
+      '[hoge] hi! hello!',
+      '[fuga] hi! how are you?'
+    ]
+  end
+
+  def broadcast message
+    @@clients.each do |client_ip|
+      echo client_ip, message
+    end
+  end
+
+  private
+
+  def echo ip, message
+    puts "echo #{ip}: #{message}"
   end
 end
--- server.rb
+++ server.rb
@@ -1,3 +1,24 @@
-class Server
+require_relative './connection'
 
+class Server
+  def initialize
+    @connection = Connection.new('127.0.0.1')
+  end
+
+  def main
+    poll
+  end
+
+  private
+
+  def poll
+    loop do
+      messages = @connection.receive
+       unless messages.empty?
+         messages.each do |message|
+           @connection.broadcast message
+         end
+       end
+    end
+  end
 end
